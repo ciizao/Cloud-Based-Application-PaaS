a7a03d83445320c696a99360e99b1dc8a6e079ca
ciizao <ciizao@uce.edu.ec>
2024-11-19 00:15:19 -0500

    feat: :sparkles: Add event Listeners and button function

--- code.js
+++ code.js
@@ -20,3 +20,18 @@ function calculate(){
 
     } );
 }
+
+monedaEl_origen.addEventListener('change', calculate);
+cantidadEl_origen.addEventListener('input', calculate);
+monedaEl_destino.addEventListener('change', calculate);
+cantidadEl_destino.addEventListener('input', calculate);
+
+taza.addEventListener('click', () =>{
+    const temp = monedaEl_origen.value;
+    monedaEl_origen.value = monedaEl_destino.value;
+    monedaEl_destino.value = temp;
+    calculate();
+} );
+
+
+calculate();
